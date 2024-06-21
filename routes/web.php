0ddff93649e1f8e1dc65a8d2a6be8b8f9bde06aa
Andani Zahira Ramadhanti <andanizr@student.ub.ac.id>
2024-06-21 22:20:59 +0700

    Kelompok3

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+//iniroutes
+
 require __DIR__.'/auth.php';
